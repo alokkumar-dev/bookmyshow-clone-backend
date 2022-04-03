836d221db9fa5f442030a9936966e609884eece7
mbnaveen <navinsmb@gmail.com>
2022-04-03 16:04:56 +0530

    back

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -2,23 +2,27 @@ const express = require("express");
 
 const Movies = require("../models/movies.model");
 const router = express.Router();
-
-  router.get("/:id", async (req, res) => {
+router.get("", async (req, res) => {
     try {
      
-      if(req.params.id){
-        const movie = await Movies.findById(req.params.id)
-        .lean()
-        .exec();
-      return res.send(movie);
-      }else{
-        const movie = await Movies.find()
-        .lean()
-        .exec();
-      return res.send(movie);
-      }
   
+      const movie = await Movies.find()
+        .lean()
+        .exec();
+      return res.send(movie);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+  });
+  router.get("", async (req, res) => {
+    try {
+     
       
+  
+      const movie = await Movies.findById(req.params.id)
+        .lean()
+        .exec();
+      return res.send(movie);
     } catch (err) {
       return res.status(500).send({ message: err.message });
     }
