67a8c49c2616a30c4e3477ca3ca16ee49fc4c17a
mbnaveen <navinsmb@gmail.com>
2022-04-03 17:17:12 +0530

    ss

--- src/models/bag.model.js
+++ src/models/bag.model.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const bagSchema = new mongoose.Schema({
-  userid: {type:Number,required:true},
-  title: {type:String,required:true},
-  img:{type:String,required:true},
-  amount:{type:Number,required:true}
+  userid: {required:true},
+  title: {required:true},
+  img:{required:true},
+  amount:{required:true}
 });
 
 const Bag = mongoose.model("bag", bagSchema); // user => users
